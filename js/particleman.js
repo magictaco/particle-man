bb3b0962879a720051811a0617ef39cc1f32fd91
Phil Moore <philip.moore@knexusresearch.com>
2017-04-26 22:05:58 -0400

    Added a few more comments.

--- js/particleman.js
+++ js/particleman.js
@@ -3,16 +3,16 @@ var WIND_GUST_STRENGTH_IN_PX = 600.0;
 
 var isRunning = false;
 
-// A $( document ).ready() block.
 $( document ).ready(function() {
     
    // Initialize controls /////////////////////////////////////////////////////
    
-   // Initialize the particle-effect-select.
+   // Initialize the effect drop-down.
    $("#particle-effect-select").change(function() {
       $(".game-canvas span").remove();
    });
    
+   // Initialize the pause button.
    $("#pause-button").hide().click(function() {
       isRunning = false;
       $(".game-canvas span").stop().remove();
@@ -20,11 +20,13 @@ $( document ).ready(function() {
       $("#status").text("");
    });
    
+   // Initialize the play button.
    $("#play-button").click(function() {
       isRunning = true;
       $("#pause-button, #play-button").toggle();
    });
    
+   // Initialize the wind gust button.
    $("#wind-button").click(function() {
       $(".game-canvas span").each(function() {
          var currLeft = $(this).css("left");
@@ -39,10 +41,14 @@ $( document ).ready(function() {
       });
    });
    
+   // Start the tick cycle.
    setInterval(performTick, TICK_FREQ_IN_MS);
    
 });
 
+/**
+ * Get the attributes for the currently selected effect.
+ */
 getEffectOptions = function() {
    var effect = $("#particle-effect-select option:selected");
    var fxOpts = new Object();
@@ -56,7 +62,11 @@ getEffectOptions = function() {
    return fxOpts;
 }; //fnend - getEffectOptions
 
+/**
+ * Perform a tick of the simulation.
+ */
 performTick = function() {
+   // Check that the simulation is not paused.
    if (isRunning) {
       
       var fxOpts = getEffectOptions();
@@ -69,7 +79,6 @@ performTick = function() {
       // Assign a z-index between 0 and 9.
       var zIndex = parseInt(Math.round(Math.random() * 10));
       
-      
       // Build the HTML for the new particle element.
       var newParticleHtml = "<span class=\"" + effectClass + "\" style=\"z-index:" + zIndex + "\">";
       if (fxOpts.text)
@@ -81,13 +90,20 @@ performTick = function() {
       // Position it at a random horizontal location.
       newParticle.css("left", (Math.random() * 100) + "%")
 
+      // Start the particle above the canvas.
       newParticle.css("top", "-20%");
-      newParticle.animate({
-         top: "120%"
-      }, parseInt(fxOpts.duration), "swing", function() {
-         newParticle.remove();
-      });
+      // Use standard jQuery animation to move the particle to the bottom of the canvas.
+      newParticle.animate(
+         { top: "120%" },
+         parseInt(fxOpts.duration), 
+         "swing",
+         function() {
+            // This is the callback function for after the animation is complete, which removes the particle from the DOM.
+            newParticle.remove();
+         }
+      );
       
+      // Update the status with the current particle count (this shows that the particles are being deleted after they're no longer in view).
       $("#status").text("Particle Count: " + $(".game-canvas span").length);
    } //endif - check for isRunning
    
